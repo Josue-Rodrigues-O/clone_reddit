d4576b6c2d6bf835ba4b10b7f23b003eef50fdfe
Josué Rodrigues de Oliveira <josue.rodrigues@inventsoftware.com.br>
2024-05-23 12:49:19 -0300

    Post tipo texto ok

--- Reddit.UserInterface/wwwroot/model/formatter.js
+++ Reddit.UserInterface/wwwroot/model/formatter.js
@@ -8,6 +8,6 @@ sap.ui.define([
             const MODELO_I18N = 'i18n';
             const recursos_i18n = this.getOwnerComponent().getModel(MODELO_I18N).getResourceBundle();
             return recursos_i18n.getText(chaveI18N);
-        },
+        },  
     }
 });
